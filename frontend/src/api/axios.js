b43be09b570d6d2ca80af8e06c9898c4945928ba
arunparakkal <arunaru0034@gmail.com>
2026-04-29 10:53:14 +0530

    changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,11 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const api = axios.create({ 
-  baseURL: 'https://mini-crm-backend-ij6l.onrender.com/api' 
+  baseURL: 'https://mini-crm-backend-ij6l.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("crm_token");
+  const token = localStorage.getItem('crm_token');
   if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
@@ -14,12 +14,12 @@ api.interceptors.response.use(
   (res) => res,
   (err) => {
     if (err.response?.status === 401) {
-      localStorage.removeItem("crm_token");
-      localStorage.removeItem("crm_user");
-      window.location.href = "/login";
+      localStorage.removeItem('crm_token');
+      localStorage.removeItem('crm_user');
+      window.location.href = '/login';
     }
     return Promise.reject(err);
-  },
+  }
 );
 
 export default api;
